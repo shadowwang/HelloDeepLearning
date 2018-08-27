0860cf8f247336bf5a4b7dea97987ee8728643ec
wangchun <906103494@qq.com>
2018-08-28 00:29:03 +0800

    Initial commit

---
+++ perceptron.py
@@ -0,0 +1,14 @@
+# 模拟感知机
+import numpy as np
+
+# 模拟感知机与们
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    return 1
+
+
